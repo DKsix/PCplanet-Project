ce875b9e18e9a2a068aa0bcf3819a7dd9362d95b
DKsix <dudukof110@gmail.com>
2023-08-06 22:15:07 -0300

    parte de ofertas

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const carousel = document.querySelector('.carousel');
+const cardsContainer = document.querySelector('.cards-container');
+const cards = document.querySelectorAll('.card2');
+const arrowLeft = document.querySelector('.arrow.left');
+const arrowRight = document.querySelector('.arrow.right');
+
+const cardWidth = cards[0].offsetWidth;
+let currentIndex = 0;
+
+arrowLeft.addEventListener('click', () => {
+    if (currentIndex > 0) {
+        currentIndex--;
+        updateCarouselPosition();
+    }
+});
+
+arrowRight.addEventListener('click', () => {
+    currentIndex++;
+    if (currentIndex >= cards.length - 2) {
+        currentIndex = 0;
+    }
+    updateCarouselPosition();
+});
+
+function updateCarouselPosition() {
+    const translateXValue = -currentIndex * cardWidth;
+    cardsContainer.style.transform = `translateX(${translateXValue}px)`;
+}
